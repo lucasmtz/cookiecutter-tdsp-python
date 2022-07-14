146947a3f91539ec47e6e328fe33ec5e5eebe446
lucas.moreira <lucas.moreira@makingscience.com>
2022-07-14 11:54:04 +0200

    Update setup_project.sh

--- {{cookiecutter.project_name}}/setup_project.sh
+++ {{cookiecutter.project_name}}/setup_project.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# --------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------------------
 # Update apt
+# -----------------------------------------------------------------------------------------------------------------
 sudo apt update -y
-# --------------------------------------------------------------------------------------------------------------------
 
 # -----------------------------------------------------------------------------------------------------------------
 # Python venv and pip
@@ -17,8 +17,7 @@ sudo apt install -y git git-flow
 # -----------------------------------------------------------------------------------------------------------------
 # Create virtual enviroment
 # -----------------------------------------------------------------------------------------------------------------
-echo
-echo "Creating virtual environment..."
+echo -e "\nCreating virtual environment..."
 rm -rf venv
 python3 -m pip install --upgrade pip setuptools wheel build
 python3 -m venv venv --upgrade-deps
@@ -26,45 +25,47 @@ chmod -R 777 venv/
 source venv/bin/activate
 
 # -----------------------------------------------------------------------------------------------------------------
+# Start a git repository if it not exists and initialize git flow
+# -----------------------------------------------------------------------------------------------------------------
+if ! git status &>/dev/null; then
+    echo -e "\nInitializing new git repository..."
+    git init -b main
+    git add .
+    git commit -m "Create {{cookiecutter.project_name}} tdsp template repository"
+    echo -e "\nInitializing git flow..."
+    git flow init -d -t 'v'
+fi
+
+# -----------------------------------------------------------------------------------------------------------------
 # Install versioneer
 # -----------------------------------------------------------------------------------------------------------------
 echo
 echo "Installing versioneer..."
 pip3 install versioneer
+if ! test -f "versioneer.py"; then
+    echo -e "\nFinishing versioneer installation..."
+    versioneer install
+fi
 
 # -----------------------------------------------------------------------------------------------------------------
-# Start a git repository if it not exists and install the project in editable mode
+# Install project in editable mode
 # -----------------------------------------------------------------------------------------------------------------
-if ! git status &>/dev/null; then
-    echo
-    echo "Initializing new git repository..."
-    git init -b main
-    echo
-    echo "Finishing versioneer installation..."
-    versioneer install
-    echo
-    echo "Installing project in editable mode..."
-    pip3 install -e ".[dev]"
-    echo
-    echo "Installing pre-commit and doing the first commit..."
-    pre-commit install
-    pre-commit autoupdate
-    git add .
-    pre-commit run --all-files
-    git add .
-    pre-commit run --all-files
-    git add .
-    pre-commit run --all-files
-    git add .
-    git commit -m "Initial commit"
-    echo
-    echo "Initializing git flow..."
-    git flow init -d -t 'v'
-else
-    echo
-    echo "Installing project in editable mode..."
-    pip3 install -e ".[dev]"
-fi
+echo -e "\nInstalling project in editable mode..."
+pip3 install -e ".[dev]"
+
+# -----------------------------------------------------------------------------------------------------------------
+# Run pre-commit hooks
+# -----------------------------------------------------------------------------------------------------------------
+echo -e "\nRunning pre-commit hooks..."
+pre-commit install
+pre-commit autoupdate
+git add .
+pre-commit run --all-files
+git add .
+pre-commit run --all-files
+git add .
+pre-commit run --all-files
+git add .
 
 # -----------------------------------------------------------------------------------------------------------------
 # Check versioneer
@@ -75,5 +76,4 @@ python setup.py version
 # -----------------------------------------------------------------------------------------------------------------
 # Success message
 # -----------------------------------------------------------------------------------------------------------------
-echo
-echo "SUCCESS! {{cookiecutter.project_name}} repository is ready to use."
+echo -e "\nSUCCESS! {{cookiecutter.project_name}} is ready to use."
